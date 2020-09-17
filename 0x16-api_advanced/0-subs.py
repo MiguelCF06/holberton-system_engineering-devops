2a45a0c460366fb9c8a49c95e279f28e7755848c
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-09-17 17:12:26 +0000

    [Update]First Update

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and returns the number of subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers"""
+    if subreddit is None or subreddit is not str:
+        return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers={"User-Agent": "miguel_cf"}).json()
+    subs = response.get("data").get("subscribers")
+    return subs
